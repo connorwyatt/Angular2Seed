6cdd7cc3cb067e906d851eced30db20d3c20930c
Connor Wyatt <connor.wyatt@midlandhr.co.uk>
2016-09-20 22:11:17 +0100

    Fixed issue where moduleId caused a zone.js console error.

--- src/app/App.component.ts
+++ src/app/App.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  moduleId: module.id,
   selector: 'app',
   template: require('./App.component.html'),
   styles: [require('./App.component.scss')]
