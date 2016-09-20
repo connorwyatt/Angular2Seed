d4a05048bfae8bea5f1bdc3491558bb41d25a562
Connor Wyatt <connor.wyatt@midlandhr.co.uk>
2016-09-20 22:09:05 +0100

    Initial commit with seed project using Angular v2.0.0

---
+++ src/app/App.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app',
+  template: require('./App.component.html'),
+  styles: [require('./App.component.scss')]
+})
+export class AppComponent {
+}
